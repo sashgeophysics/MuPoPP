7ac043d10bb990808b94cd5aa1ce242ed43922d6
YIZHUO SUN <38796475+yizhuo01@users.noreply.github.com>
2018-10-24 16:40:13 +0100

    Modify the method to define initial conditions

--- src/run/LVL_RII/LVL_RII.py
+++ src/run/LVL_RII/LVL_RII.py
@@ -19,11 +19,46 @@ from mupopp import *
 parameters["std_out_all_processes"]=False
 #set_log_level(30) #Critical info only
 ####################################
-def boundary_value(n):
-    if n < 10:
-        return float(n)/10.0
-    else:
-        return 1.0
+
+# Parameters for initializing the object
+Da0  = 10.0
+phi0 = 0.01
+Pe0  = 1.0e6
+cfl0 = 0.1
+
+# Parameters for iteration
+T0 = 1000.0
+dt0 = 0.10
+out_freq0 = 10
+
+# Parameters for mesh
+mesh_density = 60
+
+# Output parameters
+def output_write(mesh_density,Da,phi,Pe,cfl,fname="output/parameters.out"):
+    """This function saves the output of iterations"""
+    file=open(fname,"a")
+    file.write("####################################")
+    file.write("\n")
+    file.write("Mesh density:  %g\n" %mesh_density)
+    file.write("Da:  %g\n" %Da0)
+    file.write("phi:  %g\n" %phi0)
+    file.write("Pe:  %g\n" %Pe0)
+    file.write("cfl:  %g\n" %cfl0)
+    file.write("####################################")
+    file.close
+
+output_write(mesh_density,Da0,phi0,Pe0,cfl0)
+
+# Output files for quick visualisation
+output_dir     = "output/"
+extension      = "pvd"   # "xdmf" or "pvd"
+
+velocity_out   = File(output_dir + "velocity." + extension, "compressed")
+pressure_out   = File(output_dir + "pressure." + extension, "compressed")
+c0_out         = File(output_dir + "concentration0." + extension, "compressed")
+c1_out         = File(output_dir + "concentration1." + extension, "compressed")
+initial_c0_out = File(output_dir + "initial_c0." + extension, "compressed")
     
 # Define function G such that u \cdot n = g
 class BoundarySource(Expression):
@@ -38,22 +73,29 @@ class BoundarySource(Expression):
         values[1] = g*n[1]
     def value_shape(self):
         return (2,)
+
 #Define a function h such that c(x,0)=f
 class BoundaryConcentration(Expression):
-    def __init__(self, mesh,element):
+    def __init__(self, mesh, element):
         self.mesh = mesh
         self.element=element
     def eval_cell(self, values, x, ufl_cell):
         f = 0.5+0.5*(1.0-tanh(0.0/0.2))*sin(2.0*x[0]*3.14)
-        values[0] = f
-        
+        values[0] = f       
     def value_shape(self):
         return (1,)
 
-xmin=0.0
-xmax=4.0
-ymin=0.0
-ymax=1.0
+############################
+## Numerical solution
+############################
+
+# Define the mesh
+xmin = 0.0
+xmax = 4.0
+ymin = 0.0
+ymax = 1.0
+domain = Rectangle(Point(xmin,ymin),Point(xmax,ymax))
+mesh   = generate_mesh(domain,mesh_density)
     
 # Define essential boundary
 def top_bottom(x):
@@ -67,118 +109,115 @@ def top(x):
 class PeriodicBoundary(SubDomain):
     # Bottom boundary is "target domain" G
     def inside(self, x, on_boundary):
-        return bool( near(x[0], xmin) and on_boundary)
-    
+        return bool(near(x[0], xmin) and on_boundary)   
     # Map right boundary (H) to left boundary (G)
     def map(self, x, y):
         if near(x[0], xmax):
             y[0] = x[0] - xmax
-            y[1] = x[1] 
-            
+            y[1] = x[1]            
         else:
             y[0] = -1000
             y[1] = -1000
 # Create periodic boundary condition
 pbc = PeriodicBoundary()            
 
-domain=Rectangle(Point(xmin,ymin),Point(xmax,ymax))
-mesh=generate_mesh(domain,60)
-
-# Output files for quick visualisation
-output_dir     = "output/"
-extension      = "pvd"   # "xdmf" or "pvd"
-
-velocity_out   = File(output_dir + "velocity." + extension, "compressed")
-pressure_out   = File(output_dir + "pressure." + extension, "compressed")
-c0_out          = File(output_dir + "concentration0." + extension, "compressed")
-c1_out          = File(output_dir + "concentration1." + extension, "compressed")
-initial_c0_out   = File(output_dir + "initial_c0." + extension, "compressed")
+# Create FunctionSpaces
 # Velocity
 V = VectorElement("Lagrange", mesh.ufl_cell(), 2)
 # Pressure
 Q = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 # Make a mixed space
-W=dolfin.FunctionSpace(mesh, MixedElement([V,Q]))#, constrained_domain=pbc)
+W = dolfin.FunctionSpace(mesh, MixedElement([V,Q]))#, constrained_domain=pbc)
 
+# Define boundary conditions
 G=BoundarySource(mesh,element=V)
 bc1 = DirichletBC(W.sub(0), G, bottom)
 bc2 = DirichletBC(W.sub(0), Constant((0.0,0.001)), top)
-bc=[bc1,bc2]
+bc  = [bc1,bc2]
 
-##############Create an object
-darcy=DarcyAdvection(Da=10.0,phi=0.01,Pe=1.0e4,cfl=0.1)
+###########################
+## Create an object
+###########################
+darcy = DarcyAdvection(Da=Da0,phi=phi0,Pe=Pe0,cfl=cfl0)
 
-########################
+###########################
 ## Solve for Darcy velocity
 ###########################
-a,L=darcy.darcy_bilinear(W,mesh)
-sol=Function(W)
+a,L = darcy.darcy_bilinear(W,mesh)
+sol = Function(W)
 solve(a==L,sol,bc)
 
-
-u,p=sol.split()
+u,p = sol.split()
 
 velocity_out << u
-
 pressure_out << p
+
 ############################
 ## Concentrations
 ############################
-# Create finite elements
-Qc = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-# Make a mixed space containing two concentrations
-Q1=dolfin.FunctionSpace(mesh, MixedElement([Qc,Qc]),constrained_domain=pbc)
-X = FunctionSpace(mesh,"CG",1, constrained_domain=pbc)
-V1= VectorFunctionSpace(mesh,"CG",2, constrained_domain=pbc)
-vel=Function(V1)
-vtemp=Function(V1)
+# Create FunctionSpaces
+Qc = FiniteElement("Lagrange",mesh.ufl_cell(), 1)
+QM = dolfin.FunctionSpace(mesh, MixedElement([Qc,Qc]),constrained_domain=pbc)
+X  = FunctionSpace(mesh,"CG",1, constrained_domain=pbc)
+Vc = VectorFunctionSpace(mesh,"CG",2, constrained_domain=pbc)
+
+# Define the constant velocity
+vel = Function(Vc)
+vtemp = Function(Vc)
 vtemp.interpolate(Expression(("0.0","0.001"),degree=2))
-vel = vtemp #.interpolate(vtemp)
+vel = vtemp
 
 # Create functions for initial conditions
-c0 = Function(Q1)
-c00,c01=c0.split()
-temp = Function(X)
-temp.interpolate(Expression("0.5+0.5*(1.0-tanh(x[1]/0.2))*sin(2.0*x[0]*3.14)",degree=2))
-c00=temp
-c00.rename("[CO3]","")
-initial_c0_out << c00
-temp.interpolate(Expression("0.1",degree=1))
-c01=temp
-# Write the initial concentrations into files
+c0 = Function(QM)
+#c00,c01=c0.split()
 
+# Functions for components
+temp1 = Function(X)
+temp1.interpolate(Expression("0.5+0.5*(1.0-tanh(x[1]/0.2))*sin(2.0*x[0]*3.14)",degree=2))
+c00 = temp1
+temp2 = Function(X)
+temp2.interpolate(Expression("0.1",degree=1))
+c01 = temp2
 
-# Parameters
-T = 1000.0
-darcy.dt = 0.10
-t = darcy.dt
+# Assign the components
+assign(c0.sub(0), c00)
+assign(c0.sub(1), c01)
 
 # Set up boundary conditions for component 0
 c0_bottom = BoundaryConcentration(mesh,element=Qc)
-bc2 = DirichletBC(Q1.sub(0), c0_bottom,bottom)
-#bc1 = DirichletBC(Q1.sub(0), Constant(0.0),top)
-#bc2 = DirichletBC(Q1.sub(0), Constant(0.5),bottom)
+bc1 = DirichletBC(QM.sub(0), Constant(0.0),top)
+bc2 = DirichletBC(QM.sub(0), c0_bottom,bottom)
+
 # Set up boundary conditions for component 1
-#bc3 = DirichletBC(Q1.sub(1), Constant(0.1),top)
-bc4 = DirichletBC(Q1.sub(1), Constant(0.1),bottom)
+bc3 = DirichletBC(QM.sub(1), Constant(0.1),top)
+bc4 = DirichletBC(QM.sub(1), Constant(0.1),bottom)
 
-#bc_c=[bc1,bc2,bc3,bc4]
-bc_c=[bc2]#,bc4]
-sol_c=Function(Q1)
+#bc_c = [bc1,bc2,bc3,bc4]
+bc_c = [bc2]
+
+###########################
+## Solve for Concentrations
+###########################
+sol_c = Function(QM)
+
+# Parameters for iteration
+T = T0
+darcy.dt = dt0
+t = darcy.dt
 
 
-i=1
-out_freq=10
-#Save the dt and time values in an array
-dt_array=[]
-time_array=[]
+i = 1
+out_freq = out_freq0
+# Save the dt and time values in an array
+dt_array = []
+time_array = []
 while t - T < DOLFIN_EPS:
 
-    #Update the concentration of component 0
-    a1,L1=darcy.advection_diffusion_two_component(Q1,c0,vel,mesh)
-    solve(a1==L1,sol_c,bc_c)
+    # Update the concentration of component 0
+    ac,Lc = darcy.advection_diffusion_two_component(QM,c0,vel,mesh)
+    solve(ac==Lc,sol_c,bc_c)
     c0 = sol_c
-    c00,c01=sol_c.split()
+    c00,c01 = sol_c.split()
     if i % out_freq == 0:
         c00.rename("[CO3]","")
         c0_out << c00

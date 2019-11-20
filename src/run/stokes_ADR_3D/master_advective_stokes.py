969f1b594393a2f4103d088000fb5bee6abe5107
Ryan-Payton <44227248+Ryan-Payton@users.noreply.github.com>
2019-11-20 17:28:31 +0000

    Removed unnecessary functions and commands, added comments

--- src/run/stokes_ADR_3D/master_advective_stokes.py
+++ src/run/stokes_ADR_3D/master_advective_stokes.py
@@ -7,49 +7,47 @@ import numpy, scipy, sys, math
 
 from mupopp import *
 
+# Add a reference start time for time keeping
 import time
 start_time = time.time()
 
-#####################################################
+# Reduce command line output to 1 processor
 parameters["std_out_all_processes"]=False
-#set_log_level(30) #Critical info only
-#####################################################
 
 #############################
 ##   Define Input Values   ##
 #############################
 
-# Parameters for initializing the object (stokes = StokesAdvection)
-Da0  = 1.0e-2
+# Parameters for initialising the object of the class (stokes = StokesAdvection)
+Da0  = 1.0e-1
 Pe0  = 1.0e-1
-# beta = alpha0/phi0
-cfl0 = 0.1
-# Initial CO3 2- in the system
+# Initial CO3 2- mass fraction in the system
 c01_init=Expression("0.0",degree=1)
-# Initial Ca in the system
+# Initial anorthite (Ca) mass fractiona in the system
 ca_init=Expression("0.05",degree=1)
 # Initial velocity
 v_init=Expression(("0.0","0.0","0.0"),degree=3)
 
 # Parameters for iteration (aim for 100 image outputs from the simulation: T0/out_freq0 = 100)
-# T0 = L/u0 - this should be the time for movement from in to out if a straight line and constant specified velocity - actual time to travel from in to out may be longer due to tortuosity and diffusion dominant flow
-# dt0 = unitless timestep to break up T0
+# Non dimensional time, T0 can be calculated using: T0 = L/u0 (L=characteristic length, u0 = velocity)
+# T0 is calculated in seconds, this can then be used to work out how long to run the simulation as:
 
-# T0 = L/u0 = 1.3e-3/1e-7 = 13,000 seconds (3hrs 37 mins)
-# 13000/pi * 10e7 = 0.0004 years (t0=1)
-#T0 = 1 = 3hrs 37mins
-#T0 = 100 = 15 days
-#T0 = 1000 = 150 days
-#T0 = 10000 = 1500 days = 4 years 40 days
+# T0 = 1e-3/1e-7 = 10,000 seconds 
+# T0 = 1 = 2 hrs 46 mins 40 secs
+# T0 = 10 = 1 day 3 hrs 46 mins 40 secs
+# T0 = 100 = 11 days 13 hrs 46 mins 40 secs
+# T0 = 1,000 = 115 days 17 hrs 46 mins 40 secs
+# T0 = 10,000 = 3 years 62 days 9 hrs 46 mins 40 secs
+# T0 = 100,000 = 31 years 259 days 1 hr 46 mins 40 secs
 
-# T0 = L/u0 = 1.3e-3/1e-2 = 0.13 seconds
-# T0 = 1 = 0.13 seconds
-# T0 = 1000 = 130 seconds
-# T0 = 10000 = 22 minutes
-# T0 = 100000 = 3hrs 37 mins
+# To work out years from seconds you can also do:
+# seconds/pi * 10e7
 
-T0 = 10000
+# Total simulation time
+T0 = 100000
+# Time step size
 dt0 = 100
+# Every n timesteps data will be output
 out_freq0 = 1
 
 ######################
@@ -57,7 +55,7 @@ out_freq0 = 1
 ######################
 
 # Output files for visualisation
-file_name      = "BFS2_Da_%.1E_Pe_%.1E"%(Da0,Pe0) #Automatically names the output files
+file_name      = "Da_%.1E_Pe_%.1E"%(Da0,Pe0)
 output_dir     = file_name + "_output/"
 extension      = "pvd"
 
@@ -66,35 +64,6 @@ pressure_out   = File(output_dir + file_name + "_pressure." + extension, "compre
 c0_out         = File(output_dir + file_name + "_concentration0." + extension, "compressed")
 c1_out         = File(output_dir + file_name + "_concentration1." + extension, "compressed")
 c2_out         = File(output_dir + file_name + "_concentration2." + extension, "compressed")
-
-# Output parameters
-def output_write(Da,Pe,cfl,fname= output_dir + "/a_parameters.out"):
-    """This function saves the output of iterations"""
-    file=open(fname,"a")
-    file.write("####################################")
-    file.write("\n")
-    file.write("Da:  %g\n" %Da0)
-    file.write("Pe:  %g\n" %Pe0)
-    file.write("cfl:  %g\n" %cfl0)
-    file.write("####################################")
-    file.close
-
-output_write(Da0,Pe0,cfl0)
-
-##################################
-##   Create Complex Functions   ##
-##################################
-
-#Define a function h such that c(x,0)=f
-class BoundaryConcentration(Expression):
-    def __init__(self, mesh, element):
-        self.mesh = mesh
-        self.element=element
-    def eval_cell(self, values, x, ufl_cell):
-	f = 0.05*(1.0-tanh(0.0/0.2))/2.0
-        values[0] = f       
-    def value_shape(self):
-        return (1,)
 
 ###########################################
 ##   Mesh Generation + Function Spaces   ##
@@ -109,13 +78,12 @@ mesh = Mesh()
 hdf = HDF5File(mesh.mpi_comm(), "hdf5_" + mesh_fname + extension, "r")
 hdf.read(mesh, "/mesh", False)
 
-# Read in the boundaries in .h5 format, called as 'boundaries' to allow for specifying 1, 2, 3 as
+# Read in the boundaries, called as 'boundaries' to allow for specifying 1, 2, 3 as
 # inflow, outflow, walls in the below boundary conditions
-#boundaries = FacetFunction("size_t", mesh) # Works but has been depricated in 2017.2.0 and replaced as below
 boundaries = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
 hdf.read(boundaries, "/boundaries")
 
-# Define ds (2D surface for flux) and n (unit vector normal to a plane)
+# Define ds (2D surface for flux) and n (unit vector normal to a plane) for calculated input carbon flux
 ds = Measure("ds", domain = mesh, subdomain_data = boundaries)
 n = FacetNormal(mesh)
 
@@ -125,14 +93,15 @@ n = FacetNormal(mesh)
 V = VectorElement("Lagrange", mesh.ufl_cell(), 3)
 # Pressure
 Q = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-#Concentration
+# Concentrations
 Qc = FiniteElement("Lagrange",mesh.ufl_cell(), 1)
 
-# Make a mixed FunctionSpace for all 5 unknown to be calculated in together (velocity, pressure, 3x concentrations(carbonate, Ca and precipitated C)
+# Make a mixed FunctionSpace for all 5 unknowns to be calculated in together
+# (velocity, pressure, 3x concentrations (carbonic acid, anorthite and precipitated C)
 W = dolfin.FunctionSpace(mesh, MixedElement([V,Q,Qc,Qc,Qc]))
 
-# Make a FunctionSpace for initial concentration to operate in
-X  = FunctionSpace(mesh,"CG",1)
+# Make a FunctionSpace for initial concentrations to operate in
+X = FunctionSpace(mesh,"CG",1)
 
 # Make a FunctionSpace for initial velocity to operate in
 Z = VectorFunctionSpace(mesh,"CG",3)
@@ -141,21 +110,20 @@ Z = VectorFunctionSpace(mesh,"CG",3)
 ##   Boundary Conditions   ##
 #############################
 
-# Specify the file from which the boundary regions are being imported
-# 1 = inflow, 2 = outflow, 3 = walls as per enGrid
+# 1 = inflow, 2 = outflow, 3 = walls as labelled in enGrid
 
 #W.sub() substitutes one of the 5 components of the mixed function space
 #W.sub(0) uses the first component, V to then define the bc for velocity
 
 bc1 = DirichletBC(W.sub(0), Constant((1.0,0.0,0.0)), boundaries, 1) #from file: 1 = inflow
+# Velocity is defined using Pe as we calculate this based on dimensional velocity
+# therefore we set velocity to 1.0 here to retain the values 'stored' within Pe
 
-bc2 = DirichletBC(W.sub(0), Constant((0.0,0.0,0.0)), boundaries, 3) #from file: 3 = walls, 0 velocity = no flow
+bc2 = DirichletBC(W.sub(0), Constant((0.0,0.0,0.0)), boundaries, 3) #from file: 3 = walls
+# 0 velocity on the walls imposes no-flow conditions
 
-# Define c0_input as the BoundaryConcentration function for the concentration functionspace, Qc
-c0_input = BoundaryConcentration(mesh,element=Qc)
-
-#bc3 = DirichletBC(W.sub(2), c0_input, boundaries, 1) #from file: 1 = inflow
-bc3 = DirichletBC(W.sub(2), Constant(0.025), boundaries, 1) #from file: 1 = inflow
+bc3 = DirichletBC(W.sub(2), Constant(0.1), boundaries, 1) #from file: 1 = inflow
+# 0.1 (10%) is the mass fraction of the total system which is carbonic acid entering from inflow
 
 # Collect the boundary conditions
 bc = [bc1,bc2,bc3]
@@ -164,39 +132,51 @@ bc = [bc1,bc2,bc3]
 ## Initial Conditions  ##
 #########################
 
-# The initial solution is a function of the mixed function space with all 4 unknowns, W
+# The initial solution is a function of the mixed function space with all 5 unknowns, W
 sol_initial = Function(W)
 
 # Functions for components
 
-temp2 = Function(X) # Define a function in the function space X
-temp2.interpolate(c01_init) # Use the predefined expression for c01_init as the function
-c01 = temp2 # Call the initial concentration of CO3 2- c01
-c02 = temp2 # Call the initial concentration of solid precipitated C c02
-temp3 = Function(Z) # Define a function in the function space Z
-temp3.interpolate(v_init) # Use the predefined expression for v_init as the function
-vel = temp3 # Call the initial velocity v_init
-temp4 = Function(X) # Define a function in the function space X
-temp4.interpolate(ca_init) # Use the predefined expression for ca_init as the function
-c0ca = temp4 # Call the initial concentration of Ca c0ca
-# Assign the initial conditions to the correct parts of the mixed function space
-# .sub(2) inserts initial concentration (3rd value in W)
-# .sub(3) inserts initial concentration of C (4th value in W)
-# .sub(4) inserts initial concentration of C (5th value in W)
-# .sub(0) inserts initial velocity (1st value in W)
+# Define a function in the function space X and use the 
+# predefined expression for c01_init as the function
+temp1 = Function(X) 
+temp1.interpolate(c01_init)
+# As there is the same initial amount of carbonic acid (c01) and precipitated C (c02)
+# in the system we use the c01_init function and assign it to both
+c01 = temp1
+c02 = temp1
+
+# Define a function in the function space Z and use the 
+# predefined expression for v_init as the function
+temp2 = Function(Z)
+temp2.interpolate(v_init)
+# Call the initial velocity, vel and assign it the value of v_init
+vel = temp2
+
+# Define a function in the function space X and use the 
+# predefined expression for ca_init as the function
+temp3 = Function(X)
+temp3.interpolate(ca_init)
+# Call the initial concentration of anorthite, c0ca and assign it the value of ca_init
+c0ca = temp3
+
+# Assign the initial conditions to the correct parts of the mixed function space 
+# of the initial solution, sol_initial
+# .sub(x) inserts the following argument into the relevant slot of sol_initial
+# which uses the W function space so 0 = velocity, 1 = pressure, 2 = [carbonic acid] etc...
+assign(sol_initial.sub(0), vel)
 assign(sol_initial.sub(2), c01)
 assign(sol_initial.sub(3), c0ca)
 assign(sol_initial.sub(4), c02)
-assign(sol_initial.sub(0), vel)
 
 ###############
 ##  Solving  ##
 ###############
 
-# Create the darcy object from the mupopp module using the predefined values at the top
-stokes = StokesAdvection(Da=Da0,Pe=Pe0,cfl=cfl0)
+# Create the stokes object from the mupopp module using the predefined values at the top
+stokes = StokesAdvection(Da=Da0,Pe=Pe0)
 
-# The solution, sol is a function of the mixed function space for all 5 unknowns, W
+# The solution, sol is a function of the mixed function space, W for all 5 unknowns
 sol=Function(W)
 
 # Define the iteration parameters from the top
@@ -206,65 +186,83 @@ t = dt
 
 # Empty arrays to be filled with flux values and time values at each time step
 flux=np.array([])
-flux3=np.array([])
 mass=np.array([])
 time_array=np.array([])
-avg_conc=np.array([])
+avg_mass=np.array([])
 
 one = Expression(("1.0"),degree=1)
-temp5 = Function(X)
-temp5.interpolate(one)
+temp4 = Function(X)
+temp4.interpolate(one)
 
 # Iteration number and definition of output frequency for a result from the top
 i = 1
 out_freq = out_freq0
 
 # So long as the time step - total time is less than 0 perform the calculation
-# Every iteration adds dt0 to t, so when t gets to be equal to the total time specified, T it will stop
-# dt0 = 0.01
-# Initially t = dt so t = 0.01, then each timestep: t=0.02, t=0.03...
+# Every iteration adds dt0 to t, so when t gets to be equal to the total time specified,
+# T it will stop. If dt0 = 0.01 initially t = dt so t = 0.01, then each timestep: t=0.02, t=0.03...
 while t - T < DOLFIN_EPS:
-    # We solve for a and L defined in the module by the d_a_r_p_r function
-    # We provide the mixed function space, mesh, time step size, source term, initial conditions and flow direction
-    a,L=stokes.stokes_advection_diffusion_three_component(W,mesh,sol_prev=sol_initial,dt=dt0,K=0.1,zh=Constant((1.0,0.0,0.0)))
-    # Solve for a and L to get the solution, sol which contains all 5 values for the given boundary conditions
-    #solve(a==L,sol,bc)
-    solve(a==L,sol,bc,solver_parameters={'linear_solver':'mumps'})
-    # Give the solution to the function of the shared function space 
+    # We solve for a and L defined as the linear and bilinear part of F in the module function
+    # We provide the mixed function space, mesh, initial conditions, time step size and flow direction
+    a,L=stokes.stokes_ADR_precipitation(W,mesh,sol_prev=sol_initial,dt=dt0,zh=Constant((1.0,0.0,0.0)))
+    # Solve for a and L to get the solution, sol which contains all 5 values for the given BCs
+    solve(a==L,sol,bc,solver_parameters={'linear_solver':'mumps'}) # MUMPS allows for larger memory usage
+    # Set the initial conditions for the next time step as the solution to the previous step 
     sol_initial = sol
-    # Split the solution into the 4 parts of the mixed function space: velcoity, pressure, conc0, conc1
+    # Split the solution into the 5 parts of the mixed function space:
+    # velocity, pressure, [carbonic acid], [anorthite], [carbonate precipitate]
     u,p,c00,c01,c02 = sol.split()
-    # Write the results to the files and rename the result. a = CO3 2-, b = Ca, c = solid precipitated C
+    # Write the results to the files and rename the results
     if i % out_freq == 0:
 	u.rename("Velocity","")
 	velocity_out << u
 	p.rename("Pressure","")
 	pressure_out << p
-        c00.rename("[a]","")
+        c00.rename("[H2CO3]","")
         c0_out << c00
-        c01.rename("[b]","")
+        c01.rename("[Anorthite]","")
         c1_out << c01
-	c02.rename("[C]","")
+	c02.rename("[CaCO3]","")
         c2_out << c02
-	# Calculate CO3 2- input flux
+	# Calculate H2CO3 input flux, integrated over the inflow from file: 1, using ds defined above
         flux1 = assemble(c00*dot(u, -n)*ds(1))
-        flux = np.append(flux,flux1)
-        time_array=np.append(time_array,t)
-	# Calculate C mass precipitated in the domain
+	# Store the input flux at each step, flux1 in the empty array, flux        
+	flux = np.append(flux,flux1)
+
+	# Store the total time at each time step, t in the empty array, time_array        
+	time_array=np.append(time_array,t)
+
+	# Calculate C mass precipitated in the whole domain, dx
         mass1 = assemble(c02*dx)
+	# Store the total precipitated mass at each step, mass1 in the empty array, mass
         mass = np.append(mass,mass1)
-	avg_conc1 = assemble(c02*dx)/assemble(temp5*dx)
-	avg_conc = np.append(avg_conc,avg_conc1)
-    # Move to next interval and adjust boundary condition
+	
+        # Calculate the average mass precipitated at a given point in the whole domain, dx
+	avg_mass1 = assemble(c02*dx)/assemble(temp4*dx)
+	# Store the total average mass precipitated at each step, avg_conc1 in the empty array, avg_conc
+	avg_mass = np.append(avg_mass,avg_mass1)
+
+    # Move to the next time step
     info("time t =%g\n" %t)
     info("iteration =%g\n" %i)
-    #print 'iteration',i
     t += dt
     i += 1
-    flux_file_CO3=output_dir + file_name + "_CO3_flux.csv"
+    # Write the flux and mass precipitated data out:
+    # Create the .csv file for the input flux measurement
+    flux_file_CO3=output_dir + file_name + "_CO3_influx.csv"
+    # Write the time array as 1 row and the input flux measurement array as another row
     np.savetxt(flux_file_CO3,(time_array,flux),delimiter=',')
-    mass_file_C=output_dir + file_name + "_C_mass.csv"
-    np.savetxt(mass_file_C,(time_array,mass,avg_conc),delimiter=',')
+    # Create the .csv file for the total precipitated mass measurement
+    mass_file_C=output_dir + file_name + "_C_mass_total.csv"
+    # Write the time array as 1 row and the total mass precipitated measurement array as another row
+    np.savetxt(mass_file_C,(time_array,mass),delimiter=',')
+    # Create the .csv file for the average mass precipitated measurement
+    avg_mass_file_C=output_dir + file_name + "_C_mass_avg.csv"
+    # Write the time array as 1 row and the average mass precipitated measurement array as another row
+    np.savetxt(avg_mass_file_C,(time_array,avg_mass),delimiter=',')
+    
+    # Print out the total elapsed time after each time step
     info("Time elapsed %s seconds ---" % (time.time() - start_time))
 
+# Print total simulation time once complete
 info("Finished execution in %s seconds ---" % (time.time() - start_time))
